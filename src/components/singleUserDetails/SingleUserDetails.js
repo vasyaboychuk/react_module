2f68b60cb919e86b29e2b9d87f7c0cbc297be8e4
vasyaboychuk <108998512+vasyaboychuk@users.noreply.github.com>
2022-10-07 16:15:20 +0300

    add lesson 7 reduxx

--- src/components/singleUserDetails/SingleUserDetails.js
+++ src/components/singleUserDetails/SingleUserDetails.js
@@ -1,11 +1,14 @@
 import css from './SingleUserDetails.module.css';
+import {useNavigate} from "react-router-dom";
 
 function SingleUserDetails({user}) {
+    const navigate=useNavigate()
     return (
         <div className={css.SingleUserDetails}>
             <div>id:{user.id}</div>
             <div>name:{user.name}</div>
             <div>username:{user.username}</div>
+            <button onClick={()=>navigate(-1)}>Back</button>
         </div>
     )
 }
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,15 +1,21 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 import css from './User.module.css';
 
+
+
 function User({user}) {
+    const navigate=useNavigate()
     return (
         <div className={css.User}>
             <div><strong>Id:</strong> {user.id}</div>
             <div><strong>Name:</strong> {user.name}</div>
             <div><strong>Username:</strong>  {user.username}</div>
             <div><strong>Email:</strong> {user.email}</div>
-            <span><Link to={user.id.toString()}>Single User</Link></span>
+            {/*<span><Link to={user.id.toString()}>Single User</Link></span>*/}
+            <button onClick={()=>{
+                navigate(user.id.toString())
+            }}>Details</button>
         </div>
     )
 }
